f53e523d56002d23da450fb17aa904aa5a6a5d38
Alejandro Escobar <alejoescobac@gmail.com>
2013-07-26 15:51:01 -0500

    El Dream first steps

---
+++ app/controllers/dream_core_controller.rb
@@ -0,0 +1,10 @@
+class DreamCoreController < ApplicationController
+  def home
+  end
+
+  def lists
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElDream::Application.config.secret_token = '20098c41c1a215367e8c59a57a485d7f03995946e3fb6b3922cb5b01692aa0319ff86aa7d8ac0091fcfcec7abd604560d1c7f2734caec173cc9ffb9fa4e6daa6'
